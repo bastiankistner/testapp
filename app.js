ee0da9259a08e60178ec192df0e2365e2fad2fe2
ide user ide_truebluepixel <bastian.kistner@googlemail.com>
2012-05-29 11:07:20 +0000

    env test

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var http = require('http');
 
+var DB_PASS = process.env.DBPASS || process.argv[2];
+var FTP_PASS = process.env.FTPPASS || process.argv[3];
+
+
 http.createServer(function(request, response) {
    response.writeHeader(200, {"Content-Type" :  "text/plain"});
-   response.end("Hello World");
+   response.end("Hello World" + " || DB = " + DB_PASS + " || FTP = " + FTP_PASS );
 }).listen(process.env.PORT);
 
