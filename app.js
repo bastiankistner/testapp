03c627751c9d624915f6b452ff943a3f8bad8903
ide user ide_truebluepixel <bastian.kistner@googlemail.com>
2012-05-29 10:51:54 +0000

    2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-var hello = "Hi man";
+var http = require('http');
+
+http.createServer(function(request, response) {
+   response.writeHeader(200, {"Content-Type" :  "text/plain"});
+   response.end("Hello World");
+}).listen(process.env.PORT);
+
